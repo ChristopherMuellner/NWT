319198e0c42f3450886b5c803d822ee287eb977f
ChristopherMuellner <119624731+ChristopherMuellner@users.noreply.github.com>
2024-11-27 20:48:02 +0100

    Create command.cmd

---
+++ command.cmd
@@ -0,0 +1,87 @@
+C:\Users\chris>ipconfig/all
+
+Windows-IP-Konfiguration
+
+   Hostname  . . . . . . . . . . . . : vujh
+   Primäres DNS-Suffix . . . . . . . :
+   Knotentyp . . . . . . . . . . . . : Hybrid
+   IP-Routing aktiviert  . . . . . . : Nein
+   WINS-Proxy aktiviert  . . . . . . : Nein
+   DNS-Suffixsuchliste . . . . . . . : fritz.box
+
+Ethernet-Adapter Ethernet:
+
+   Medienstatus. . . . . . . . . . . : Medium getrennt
+   Verbindungsspezifisches DNS-Suffix:
+   Beschreibung. . . . . . . . . . . : Realtek PCIe GbE Family Controller
+   Physische Adresse . . . . . . . . : 40-C2-BA-31-FE-46
+   DHCP aktiviert. . . . . . . . . . : Ja
+   Autokonfiguration aktiviert . . . : Ja
+
+Ethernet-Adapter vEthernet (WSL (Hyper-V firewall)):
+
+   Verbindungsspezifisches DNS-Suffix:
+   Beschreibung. . . . . . . . . . . : Hyper-V Virtual Ethernet Adapter
+   Physische Adresse . . . . . . . . : 00-15-5D-0A-D1-05
+   DHCP aktiviert. . . . . . . . . . : Nein
+   Autokonfiguration aktiviert . . . : Ja
+   Verbindungslokale IPv6-Adresse  . : fe80::7aa1:79eb:3ca4:c6c3%50(Bevorzugt)
+   IPv4-Adresse  . . . . . . . . . . : 172.22.16.1(Bevorzugt)
+   Subnetzmaske  . . . . . . . . . . : 255.255.240.0
+   Standardgateway . . . . . . . . . :
+   DHCPv6-IAID . . . . . . . . . . . : 838866269
+   DHCPv6-Client-DUID. . . . . . . . : 00-01-00-01-2C-B9-38-9D-40-C2-BA-31-FE-46
+   NetBIOS über TCP/IP . . . . . . . : Aktiviert
+
+Drahtlos-LAN-Adapter LAN-Verbindung* 3:
+
+   Medienstatus. . . . . . . . . . . : Medium getrennt
+   Verbindungsspezifisches DNS-Suffix:
+   Beschreibung. . . . . . . . . . . : Microsoft Wi-Fi Direct Virtual Adapter #3
+   Physische Adresse . . . . . . . . : D2-39-57-78-87-E3
+   DHCP aktiviert. . . . . . . . . . : Ja
+   Autokonfiguration aktiviert . . . : Ja
+
+Drahtlos-LAN-Adapter LAN-Verbindung* 4:
+
+   Medienstatus. . . . . . . . . . . : Medium getrennt
+   Verbindungsspezifisches DNS-Suffix:
+   Beschreibung. . . . . . . . . . . : Microsoft Wi-Fi Direct Virtual Adapter #4
+   Physische Adresse . . . . . . . . : D2-39-57-78-97-F3
+   DHCP aktiviert. . . . . . . . . . : Ja
+   Autokonfiguration aktiviert . . . : Ja
+
+Drahtlos-LAN-Adapter WLAN:
+
+   Verbindungsspezifisches DNS-Suffix: fritz.box
+   Beschreibung. . . . . . . . . . . : MediaTek Wi-Fi 6 MT7921 Wireless LAN Card
+   Physische Adresse . . . . . . . . : D0-39-57-78-A7-C3
+   DHCP aktiviert. . . . . . . . . . : Ja
+   Autokonfiguration aktiviert . . . : Ja
+   IPv6-Adresse. . . . . . . . . . . : 2a04:9546:19ba:af01:c52c:be1:3372:e973(Bevorzugt)
+   Temporäre IPv6-Adresse. . . . . . : 2a04:9546:19ba:af01:cd1:8efb:95ec:cc18(Bevorzugt)
+   Verbindungslokale IPv6-Adresse  . : fe80::e436:d5b3:2c27:c44e%5(Bevorzugt)
+   IPv4-Adresse  . . . . . . . . . . : 192.168.178.23(Bevorzugt)
+   Subnetzmaske  . . . . . . . . . . : 255.255.255.0
+   Lease erhalten. . . . . . . . . . : Mittwoch, 27. November 2024 19:59:32
+   Lease läuft ab. . . . . . . . . . : Samstag, 7. Dezember 2024 19:59:32
+   Standardgateway . . . . . . . . . : fe80::62b5:8dff:fe35:646d%5
+                                       192.168.178.1
+   DHCP-Server . . . . . . . . . . . : 192.168.178.1
+   DHCPv6-IAID . . . . . . . . . . . : 63977815
+   DHCPv6-Client-DUID. . . . . . . . : 00-01-00-01-2C-B9-38-9D-40-C2-BA-31-FE-46
+   DNS-Server  . . . . . . . . . . . : fd00::62b5:8dff:fe35:646d
+                                       2a04:9546:19ba:af01:62b5:8dff:fe35:646d
+                                       192.168.178.1
+   NetBIOS über TCP/IP . . . . . . . : Aktiviert
+
+Ethernet-Adapter Bluetooth-Netzwerkverbindung:
+
+   Medienstatus. . . . . . . . . . . : Medium getrennt
+   Verbindungsspezifisches DNS-Suffix:
+   Beschreibung. . . . . . . . . . . : Bluetooth Device (Personal Area Network)
+   Physische Adresse . . . . . . . . : D0-39-57-78-A7-C4
+   DHCP aktiviert. . . . . . . . . . : Ja
+   Autokonfiguration aktiviert . . . : Ja
+
+C:\Users\chris>
